c651d00d0498cf1e9755af436cd4d3b039c4efc9
lucifer <arifulhassanjoy02@gmail.com>
2025-07-10 00:23:51 +0600

    Add files via upload

---
+++ Print Hello world/Window.alert/index.js
@@ -0,0 +1,9 @@
+console.log(`Hello lucifer`);
+
+window.alert(`This is an alert`);
+window.alert("I like pizza");
+
+document.getElementById("arasso").innerHTML 
+= "saranghae elmirashi";
+document.getElementById("arigato").innerHTML
+= "naneun johae elmirashi";
